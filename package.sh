f0a7b37572df3835df4e6e60cd6c352e5c70c1b2
Veroline Malongfor <vmalongfor@gmail.com>
2022-10-19 16:42:20 -0400

    modified packages for alpine

--- package.sh
+++ package.sh
@@ -4,14 +4,14 @@
 #### This is a script to install packages on alpine
 
 
-apk install wget -y
-apk install net-utils -y
-apk install sysstat -y
-apk install finger -y
-apk install gcc -y
-apk install make -y
-apk install python3 -y
-apk install epel-release -y
-apk install git -y
+apk add wget 
+apk add net-utils 
+apk add sysstat 
+apk add finger 
+apk add gcc 
+apk add make 
+apk add python3 
+apk add epel-release 
+apk add git
 sleep 10
 echo "Installation of packages on alpine is completed"
