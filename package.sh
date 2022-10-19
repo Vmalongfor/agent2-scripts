c1f115206870f4403471d6f2d196218e0f5aa1f8
Veroline Malongfor <vmalongfor@gmail.com>
2022-10-19 16:26:15 -0400

    modified packages for alpine

--- package.sh
+++ package.sh
@@ -1,1 +1,17 @@
-# agent2-scripts
+#!/bin/bash
+## author: Veroline
+#### Date: 10/19/2022
+#### This is a script to install packages on alpine
+
+
+apk install wget -y
+apk install net-utils -y
+apk install sysstat -y
+apk install finger -y
+apk install gcc -y
+apk install make -y
+apk install python3 -y
+apk install epel-release -y
+apk install git -y
+sleep 10
+echo "Installation of packages on alpine is completed"
